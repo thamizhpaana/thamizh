ff013c550ee49607f07a8c936c323ef469c3d4f7
thamizhpaana <52271356+thamizhpaana@users.noreply.github.com>
2019-08-05 01:55:22 -0700

    Update 45.py

--- 45.py
+++ 45.py
@@ -1,4 +1,8 @@
-import sys, string
-ade = input()
-adi = list(map(int,input().split()))
-print(sum(adi) // len(adi))
+#TP
+n=int(input())
+l=list(map(int,input().split()))
+s=l[0:n]
+for i in range(0,n):
+    print(s[i]," ",i)
+   
+    print ("\r")
